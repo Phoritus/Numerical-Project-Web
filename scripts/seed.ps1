40ba984af5bd76778fd8024bc730107a7ea2a408
King Vagandar <poohzazazaja@gmail.com>
2025-10-12 23:44:24 +0700

    deleted unnecessary file

--- scripts/seed.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [switch]$Prod
-)
-
-if ($Prod) {
-  docker compose -f docker-compose.prod.yml --env-file .env run --rm seed
-} else {
-  docker compose run --rm seed
-}
--- scripts/up.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [switch]$Prod
-)
-
-if ($Prod) {
-  docker compose -f docker-compose.prod.yml --env-file .env up -d --build
-} else {
-  docker compose up -d --build
-}
